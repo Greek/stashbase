880a74b1eae879cf1ee1aa9e787178c03887f1ea
Andreas <git@fsck.rip>
2025-07-11 21:17:57 -0700

    fix(nanoid): return a function call instead of doing whatever we were doing there
    Signed-off-by: Andreas <git@fsck.rip>

--- apps/api/src/lib/nanoid.ts
+++ apps/api/src/lib/nanoid.ts
@@ -1,7 +1,10 @@
 import { customAlphabet } from 'nanoid';
 
-export const nanoid = (size: number) =>
-  customAlphabet(
+export const nanoid = (size?: number) => {
+  const fn = customAlphabet(
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-',
     size ?? 16,
   );
+
+  return fn();
+};
