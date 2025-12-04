c1a9aec45a34df5f08fbcd4c32b299bb58e970de
Andreas <git@fsck.rip>
2025-12-04 15:07:57 -0500

    feat(membership): add method to check user membership
    Signed-off-by: Andreas <git@fsck.rip>

--- apps/api/src/datastore/membership/index.ts
+++ apps/api/src/datastore/membership/index.ts
@@ -1,6 +1,7 @@
 import { db } from '@api/db';
 import { membership } from '@api/db/schema';
 import { nanoid } from '@api/lib/nanoid';
+import { and, eq } from 'drizzle-orm';
 import { CreateMembershipInput } from './types';
 
 export const MembershipDatastore = {
@@ -12,5 +13,19 @@ export const MembershipDatastore = {
     });
 
     return;
+  },
+  checkMembership: async (opts: { spaceId: string; userId: string }) => {
+    const res = await db
+      .select({ id: membership.id })
+      .from(membership)
+      .where(
+        and(
+          eq(membership.memberId, opts.userId),
+          eq(membership.spaceId, opts.spaceId),
+        ),
+      );
+
+    // if res; true. else false
+    return !!res;
   },
 };
